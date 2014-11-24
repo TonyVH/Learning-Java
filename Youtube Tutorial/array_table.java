af69749503f342db6529ecd90eefff5bdc0d8756
TonyVh <tonyvandenhoek@gmail.com>
2014-11-23 21:11:22 -0500

    edited 11/23/14

--- Youtube Tutorial/array_table.java
+++ Youtube Tutorial/array_table.java
@@ -1,10 +1,11 @@
 class array_table {
     public static void main(String[] args) {
-        System.out.println("Index\tValue");
+        System.out.println("Index \t Value");
         int list[] = {32,12,34,53,23,65,43};
 
         for(int i=0; i<list.length; i++) {
-            System.out.println(i + "\t" + list[i]);
+            System.out.printf("  %d \t  %d",i,list[i]);
+            System.out.println();
         }
     }
 }
