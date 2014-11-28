6a415cbb41731fcfa6cde146f20e8b17c52cf8b2
TonyVh <tonyvandenhoek@gmail.com>
2014-11-28 10:02:41 -0500

    edited 11/28/14

---
+++ Java Book/Chapter 02/CharArithDemo.java
@@ -0,0 +1,15 @@
+// Character variables can be handled like integers.
+class CharArithDemo {
+    public static void main(String args[]) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++;   // Increment ch
+        System.out.println("ch is now " + ch);
+
+        ch = 90;    // Gives ch the value of 'Z'
+        System.out.println("ch is now " + ch);
+    }
+}
