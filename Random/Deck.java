51c4768267578d5ed8b11ee3ef6ffe9b17b7af80
TonyVH <tonyvandenhoek@gmail.com>
2014-11-28 21:39:01 -0500

    edited 11/28/14

--- Random/Deck.java
+++ Random/Deck.java
@@ -3,13 +3,12 @@
     to be used in simple card games
 */
 public class Deck {
-    int deck[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,
-                  14,15,16,17,18,19,20,21,22,23,24,25,26,
-                  27,28,29,30,31,32,33,34,35,36,37,38,39,
-                  40,41,42,43,44,45,46,47,48,49,50,51,52};
-    
+    String deck[] = {"1C", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "11C", "12C", "13C",
+                     "1D", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "11D", "12D", "13D",
+                     "1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "11H", "12H", "13H",
+                     "1S", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "11S", "12S", "13S"};
 
-    int view_card(int card) {
+    String view_card(int card) {
         return deck[card];
     }
 }
@@ -18,6 +17,6 @@ public class Deck {
 class main {
     public static void main(String args[]) {
         Deck deck = new Deck();
-        System.out.println(deck.view_card(0));
+        System.out.println(deck.view_card(43));
     }
 } 
