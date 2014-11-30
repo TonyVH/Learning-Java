c69a3022ac9419670635eb9138ba80f99d443620
TonyVH <tonyvandenhoek@gmail.com>
2014-11-30 15:19:54 -0500

    edited 11/30/14

--- Java Book/Chapter 02/LogicalOpTable.java
+++ Java Book/Chapter 02/LogicalOpTable.java
@@ -7,22 +7,22 @@ class LogicalOpTable {
 
         p = true; q = true;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
        
         p = true; q = false;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
        
         p = false; q = true;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
         
         p = false; q = false;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
 
     }
--- Java Book/Chapter 02/LogicalOpTable2.java
+++ Java Book/Chapter 02/LogicalOpTable2.java
@@ -1,29 +1,29 @@
 // Try This 2-2: A truth table for the logical operators.
-class LogicalOpTable {
+class LogicalOpTable2 {
     public static void main(String args[]) {
-        boolean p, q;
+        int p, q;
 
         System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
 
-        p = true; q = true;
+        p = 1; q = 1;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + 0);
        
-        p = true; q = false;
+        p = 1; q = 0;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + 0);
        
-        p = false; q = true;
+        p = 0; q = 1;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + 1);
         
-        p = false; q = false;
+        p = 0; q = 0;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p&q) + "t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + 1);
 
     }
 }
