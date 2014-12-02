b6c1086dfcce11271cf8d08cdee1edec460a23f8
TonyVH <tonyvandenhoek@gmail.com>
2014-12-01 22:16:53 -0500

    edited 12/1/14

---
+++ Java Book/Chapter 03/ForVar.java
@@ -0,0 +1,17 @@
+// Declare loop control variable inside the for.
+class ForVar {
+    public static void main(String args[]) {
+        int sum = 0;
+        int fact = 1;
+
+        // Compute the factorial of the numbers through 5
+        for(int i = 1; i <= 5; i++) {
+            sum += i;
+            fact *= i;
+        }
+
+        // i is not know outside of the for statement
+        System.out.println("Sum is " + sum);
+        System.out.println("Factorial is " + fact);
+    }
+}
