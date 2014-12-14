199c0364d6c049a57b7ae3064317a1fd6471ac7b
TonyVH <tonyvandenhoek@gmail.com>
2014-12-14 15:26:43 -0500

    edited 12/14/14

---
+++ Java Book/Chapter 05/LengthDemo.java
@@ -0,0 +1,32 @@
+// Use the lenth array member.
+class LengthDemo {
+    public static void main(String args[]) {
+        int list[] = new int[10];
+        int nums[] = {1, 2, 3};
+        int table[][] = {
+            {1, 2, 3},
+            {4, 5},
+            {6, 7, 8, 9}
+        };
+
+        System.out.println("Length of list is " + list.length);
+        System.out.println("Length of nums is " + nums.length);
+        System.out.println("Length of table is " + table.length);
+        System.out.println("Length of table[0] " + table[0].length);
+        System.out.println("Length of table[1] " + table[1].length);
+        System.out.println("Length of table[2] " + table[2].length);
+        System.out.println();
+
+        // Use length to initialize list
+        for(int i = 0; i < list.length; i++) {
+            list[i] = i * i;
+        }
+
+        System.out.println("Here is list: ");
+        // Now use length to display list
+        for(int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
