bc9296b398cd8fbfa59dae6b14cbeb0e4e749eb0
TonyVH <tonyvandenhoek@gmail.com>
2014-12-18 21:37:43 -0500

    edited 12/18/14

---
+++ Java Book/Chapter 05/test.java
@@ -0,0 +1,9 @@
+class test {
+    public static void main(String args[]) {
+        String m = "m";
+        char m2 = 'm';
+        if(m == m2) {
+            System.out.println("true");
+        }
+    }
+}
