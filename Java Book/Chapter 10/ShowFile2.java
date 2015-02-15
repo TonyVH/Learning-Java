dfd1da2b296bd39368cec1daf96992c0d10b3c46
TonyVH <tonyvandenhoek@gmail.com>
2015-02-15 09:21:07 -0500

    edited 2/15/15

---
+++ Java Book/Chapter 10/ShowFile2.java
@@ -0,0 +1,48 @@
+/* 
+   This variation wraps the code that opens and
+   accesses the file within a single try block.
+   The file is closed by the finally block.
+*/
+
+import java.io.*;
+
+class ShowFile2 {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fileIn = null;
+
+        if(args.length < 1) {
+            System.out.println("Usage: ShowFile2 File");
+            return;
+        }
+
+        // The following code opens a file, reads characters until EOF
+        // is encountered, and then closes the file via a finally block.
+        try {
+            fileIn = new FileInputStream(args[0]);
+
+            do {
+                i = fileIn.read();
+                if(i != -1) {
+                    System.out.print((char) i);
+                }
+            } while(i != -1);
+        }
+        catch(FileNotFoundException exc) {
+            System.out.println("File Not Found");
+        }
+        catch(IOException exc) {
+            System.out.println("An I/O Error Occurred");
+        }
+        finally {
+            try {
+                if(fileIn != null) {
+                    fileIn.close();
+                }
+            }
+            catch(IOException exc) {
+                System.out.println("Error Closing File");
+            }
+        }
+    }
+}
